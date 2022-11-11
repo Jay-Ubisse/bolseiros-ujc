652f4862d3a3b876fdd2f8ca319a53683ded3441
Jay-Ubisse <joaquimubisse@gmail.com>
2022-11-11 17:20:15 +0200

    rectificando o path as rotas

--- server/controller/login.php
+++ server/controller/login.php
@@ -1,16 +1,16 @@
 <?php 
     require_once '../connection/connection.php';
-    require_once 'Usuario.php'; //Importar a classe usuario
+    require_once 'Usuario.php'; 
 
 
-        $n = new Usuario(); // instanciar um objecto da classe Usuario
+        $n = new Usuario(); 
       
 
         $utilizador = addslashes($_POST['utilizador']);
         $senha = addslashes($_POST['senha']);
       
         if($n->login($utilizador, $senha) == true){
-            header("Location: ../../client/views/pagina-inicial/");
+            header("Location: ../../client/pages/pagina-inicial/");
         } else{
            header("location: ../../");
         }
